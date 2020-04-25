1d5748228ba8b2df631b2231980f1e7dd10b506a
Kristóf Szabó <19kristof95@gmail.com>
2020-04-25 21:19:32 +0200

    Data's methods implemented

--- src/oophazi/Data.java
+++ src/oophazi/Data.java
@@ -8,25 +8,64 @@ import java.util.*;
 public class Data implements Serializable {
 
     /**
+     *  Sensor device that measured this Data
+     */
+    private Sensor sensor;
+
+    /**
+     *  Value of the sensor
+     */
+    private double value;
+
+    /**
+     *  The time when the value was measured
+     */
+    private Date measuredTime;
+
+    /**
      * Default constructor
      */
     public Data() {
     }
 
-    /**
-     * 
-     */
-    private Device device;
+    public Data(Sensor sensor, double value){
+        this.sensor = sensor;
+        this.value = value;
+    }
 
-    /**
-     * 
-     */
-    private double value;
 
     /**
-     * 
+     * sensor getter
+     *
+     * @return sensor
      */
-    private Date measuredTime;
+    public Sensor getSensor() {
+        return sensor;
+    }
 
 
+    /**Value getter
+     *
+     * @return value
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * measuredTime getter
+     *
+     * @return measuredTime
+     */
+    public Date getMeasuredTime() {
+        return measuredTime;
+    }
+
+    /**measuredTime setter
+     *
+     * @param measuredTime when the data was measured
+     */
+    public void setMeasuredTime(Date measuredTime) {
+        this.measuredTime = measuredTime;
+    }
 }
