7ef5782067742895a84811c244e03c453bd62747
Kristóf Szabó <19kristof95@gmail.com>
2020-04-25 22:29:24 +0200

    DataConcentrator implemented

--- src/oophazi/DataConcentrator.java
+++ src/oophazi/DataConcentrator.java
@@ -1,5 +1,7 @@
 package oophazi;
 
+import java.util.ArrayList;
+
 /**
  *
  */
@@ -8,7 +10,15 @@ public class DataConcentrator extends Device {
     /**
      * Default constructor
      */
-    public DataConcentrator() {
+    public DataConcentrator(String name) {
+        super(name);
+        ArrayList<Socket> socketsIn = getInputSockets();
+        ArrayList<Socket> socketsOut = getInputSockets();
+
+        socketsIn.add(new Socket(this));
+        for(int i = 0; i < 8;i++){
+            socketsOut.add(new Socket(this));
+        }
     }
 
 }
