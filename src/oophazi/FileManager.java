48684454554b646f63073fb27acd13a192f5125e
Kristóf Szabó <19kristof95@gmail.com>
2020-04-28 15:06:52 +0200

    FileManager implemented

--- src/oophazi/FileManager.java
+++ src/oophazi/FileManager.java
@@ -1,31 +1,36 @@
 
 package oophazi;
+import java.io.*;
 import java.util.*;
 /**
  * 
  */
 public class FileManager {
 
+    private ModelManager modelManager;
+
     /**
      * Default constructor
      */
-    public FileManager() {
+    public FileManager(ModelManager modelManager) {
+        this.modelManager = modelManager;
     }
 
     /**
      * @param fileName A létrehozandó fájl neve
      */
-    public void save(String fileName) {
-        // TODO implement here
+    public void save(String fileName) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName));
+        oos.writeObject(modelManager);
     }
 
     /**
      * @param filename A betöltendő file neve
      * @return A betöltött ModelManager példány
      */
-    public ModelManager load(String filename) {
-        // TODO implement here
-        return null;
+    public ModelManager load(String filename) throws IOException, ClassNotFoundException {
+        ObjectInputStream oin = new ObjectInputStream(new FileInputStream(filename));
+        return (ModelManager) oin.readObject();
     }
 
 }
