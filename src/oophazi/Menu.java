40dc933e46cee6bc4e1d0fab67f208e8d69da658
Kristóf Szabó <19kristof95@gmail.com>
2020-05-10 17:30:44 +0200

    Menu implemented

--- src/oophazi/Menu.java
+++ src/oophazi/Menu.java
@@ -1,4 +1,6 @@
 package oophazi;
+import oophazi.commands.*;
+
 import java.util.*;
 
 /**
@@ -6,10 +8,24 @@ import java.util.*;
  */
 public class Menu {
 
+    private HashMap<String, Command> commandHashMap;
     /**
      * Default constructor
      */
     public Menu() {
+        commandHashMap = new HashMap<>();
+        commandHashMap.put("data",new DataCommand());
+        commandHashMap.put("device", new DeviceCommand());
+        commandHashMap.put("exit", new ExitCommand());
+        commandHashMap.put("help", new HelpCommand());
+        commandHashMap.put("load", new LoadCommand());
+        commandHashMap.put("save", new SaveCommand());
+        commandHashMap.put("setdatetime", new SetDateTimeCommand());
+        commandHashMap.put("step", new StepCommand());
+    }
+
+    public void play(ModelManager modelManager, String[] cmd){
+        commandHashMap.get(cmd[0]).action(modelManager, cmd);
     }
 
 
